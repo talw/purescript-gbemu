9b7030b4d1f8a090a80e521ac66e7c05cacdc5e4
Tal Walter <talw10@gmail.com>
2016-05-15 22:09:11 +0300

    Added the extended operations map

--- src/OpcodeMap.purs
+++ src/OpcodeMap.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Types
 import Ops
+import Utils
 
 
 --Tried to use typeclasses, but was forced to turn all my type synonyms
@@ -357,4 +358,344 @@ basicOps =
   , invalidOpCode
   , mr2op compAToImm                        -- CP n
   , mm2op $ callRoutine 0x38                -- RST 38
+]
+
+extOps :: Array (Z80State -> Z80State)
+extOps = 
+                                          -- 0x
+  [ rr2op $ rotReg Left true setB b       -- RLC B
+  , rr2op $ rotReg Left true setC c       -- RLC C
+  , rr2op $ rotReg Left true setD d       -- RLC D
+  , rr2op $ rotReg Left true setE e       -- RLC E
+
+  , rr2op $ rotReg Left true setH h       -- RLC H
+  , rr2op $ rotReg Left true setL l       -- RLC L
+  , mm2op $ rotHLMem Left true            -- RLC (HL)
+  , rr2op $ rotReg Left true setA a       -- RLC A
+
+  , rr2op $ rotReg Right true setB b      -- RRC B
+  , rr2op $ rotReg Right true setC c      -- RRC C
+  , rr2op $ rotReg Right true setD d      -- RRC D
+  , rr2op $ rotReg Right true setE e      -- RRC E
+
+  , rr2op $ rotReg Right true setH h      -- RRC H
+  , rr2op $ rotReg Right true setL l      -- RRC L
+  , mm2op $ rotHLMem Right true           -- RRC (HL)
+  , rr2op $ rotReg Right true setA a      -- RRC A
+
+                                          -- 1x
+  , rr2op $ rotReg Left false setB b      -- RL B
+  , rr2op $ rotReg Left false setC c      -- RL C
+  , rr2op $ rotReg Left false setD d      -- RL D
+  , rr2op $ rotReg Left false setE e      -- RL E
+
+  , rr2op $ rotReg Left false setH h      -- RL H
+  , rr2op $ rotReg Left false setL l      -- RL L
+  , mm2op $ rotHLMem Left false           -- RL (HL)
+  , rr2op $ rotReg Left false setA a      -- RL A
+
+  , rr2op $ rotReg Right false setB b     -- RR B
+  , rr2op $ rotReg Right false setC c     -- RR C
+  , rr2op $ rotReg Right false setD d     -- RR D
+  , rr2op $ rotReg Right false setE e     -- RR E
+
+  , rr2op $ rotReg Right false setH h     -- RR H
+  , rr2op $ rotReg Right false setL l     -- RR L
+  , mm2op $ rotHLMem Right false          -- RR(HL)
+  , rr2op $ rotReg Right false setA a     -- RR A
+
+                                          -- 2x
+  , rr2op $ shiftReg Left false setB b    -- SLA B
+  , rr2op $ shiftReg Left false setC c    -- SLA C
+  , rr2op $ shiftReg Left false setD d    -- SLA D
+  , rr2op $ shiftReg Left false setE e    -- SLA E
+
+  , rr2op $ shiftReg Left false setH h    -- SLA H
+  , rr2op $ shiftReg Left false setL l    -- SLA L
+  , mm2op $ shiftMemHL Left false         -- SLA(HL)
+  , rr2op $ shiftReg Left false setA a    -- SLA A
+
+  , rr2op $ shiftReg Right true setB b    -- SRA B
+  , rr2op $ shiftReg Right true setC c    -- SRA C
+  , rr2op $ shiftReg Right true setD d    -- SRA D
+  , rr2op $ shiftReg Right true setE e    -- SRA E
+
+  , rr2op $ shiftReg Right true setH h    -- SRA H
+  , rr2op $ shiftReg Right true setL l    -- SRA L
+  , mm2op $ shiftMemHL Right true         -- SRA (HL)
+  , rr2op $ shiftReg Right true setA a    -- SRA A
+
+                                          -- 3x
+  , rr2op $ swapReg setB b                -- SWAP B
+  , rr2op $ swapReg setC c                -- SWAP C
+  , rr2op $ swapReg setD d                -- SWAP D
+  , rr2op $ swapReg setE e                -- SWAP E
+
+  , rr2op $ swapReg setH h                -- SWAP H
+  , rr2op $ swapReg setL l                -- SWAP L
+  , mm2op $ swapMemHL                     -- SWAP(HL)
+  , rr2op $ swapReg setA a                -- SWAP A
+
+  , rr2op $ shiftReg Right false setB b   -- SRL B
+  , rr2op $ shiftReg Right false setC c   -- SRL C
+  , rr2op $ shiftReg Right false setD d   -- SRL D
+  , rr2op $ shiftReg Right false setE e   -- SRL E
+
+  , rr2op $ shiftReg Right false setH h   -- SRL H
+  , rr2op $ shiftReg Right false setL l   -- SRL L
+  , mm2op $ shiftMemHL Right false        -- SRL (HL)
+  , rr2op $ shiftReg Right false setA a   -- SRL A
+
+                                          -- 4x
+  , rr2op $ testBitNOfReg 0 b             -- BIT 0,B
+  , rr2op $ testBitNOfReg 0 c             -- BIT 0,C
+  , rr2op $ testBitNOfReg 0 d             -- BIT 0,D
+  , rr2op $ testBitNOfReg 0 e             -- BIT 0,E
+
+  , rr2op $ testBitNOfReg 0 h             -- BIT 0,H
+  , rr2op $ testBitNOfReg 0 l             -- BIT 0,L
+  , mr2op $ testBitNOfHLMem 0             -- BIT 0,(HL)
+  , rr2op $ testBitNOfReg 0 a             -- BIT 0,A
+
+  , rr2op $ testBitNOfReg 1 b             -- BIT 1,B
+  , rr2op $ testBitNOfReg 1 c             -- BIT 1,C
+  , rr2op $ testBitNOfReg 1 d             -- BIT 1,D
+  , rr2op $ testBitNOfReg 1 e             -- BIT 1,E
+
+  , rr2op $ testBitNOfReg 1 h             -- BIT 1,H
+  , rr2op $ testBitNOfReg 1 l             -- BIT 1,L
+  , mr2op $ testBitNOfHLMem 1             -- BIT 1,(HL)
+  , rr2op $ testBitNOfReg 1 a             -- BIT 1,A
+
+                                          -- 5x
+  , rr2op $ testBitNOfReg 2 b             -- BIT 2,B
+  , rr2op $ testBitNOfReg 2 c             -- BIT 2,C
+  , rr2op $ testBitNOfReg 2 d             -- BIT 2,D
+  , rr2op $ testBitNOfReg 2 e             -- BIT 2,E
+
+  , rr2op $ testBitNOfReg 2 h             -- BIT 2,H
+  , rr2op $ testBitNOfReg 2 l             -- BIT 2,L
+  , mr2op $ testBitNOfHLMem 2             -- BIT 2,(HL)
+  , rr2op $ testBitNOfReg 2 a             -- BIT 2,A
+
+  , rr2op $ testBitNOfReg 3 b             -- BIT 3,B
+  , rr2op $ testBitNOfReg 3 c             -- BIT 3,C
+  , rr2op $ testBitNOfReg 3 d             -- BIT 3,D
+  , rr2op $ testBitNOfReg 3 e             -- BIT 3,E
+
+  , rr2op $ testBitNOfReg 3 h             -- BIT 3,H
+  , rr2op $ testBitNOfReg 3 l             -- BIT 3,L
+  , mr2op $ testBitNOfHLMem 3             -- BIT 3,(HL)
+  , rr2op $ testBitNOfReg 3 a             -- BIT 3,A
+
+                                          -- 6x
+  , rr2op $ testBitNOfReg 4 b             -- BIT 4,B
+  , rr2op $ testBitNOfReg 4 c             -- BIT 4,C
+  , rr2op $ testBitNOfReg 4 d             -- BIT 4,D
+  , rr2op $ testBitNOfReg 4 e             -- BIT 4,E
+
+  , rr2op $ testBitNOfReg 4 h             -- BIT 4,H
+  , rr2op $ testBitNOfReg 4 l             -- BIT 4,L
+  , mr2op $ testBitNOfHLMem 4             -- BIT 4,(HL)
+  , rr2op $ testBitNOfReg 4 a             -- BIT 4,A
+
+  , rr2op $ testBitNOfReg 5 b             -- BIT 5,B
+  , rr2op $ testBitNOfReg 5 c             -- BIT 5,C
+  , rr2op $ testBitNOfReg 5 d             -- BIT 5,D
+  , rr2op $ testBitNOfReg 5 e             -- BIT 5,E
+
+  , rr2op $ testBitNOfReg 5 h             -- BIT 5,H
+  , rr2op $ testBitNOfReg 5 l             -- BIT 5,L
+  , mr2op $ testBitNOfHLMem 5             -- BIT 5,(HL)
+  , rr2op $ testBitNOfReg 5 a             -- BIT 5,A
+
+                                          -- 7x
+  , rr2op $ testBitNOfReg 6 b             -- BIT 6,B
+  , rr2op $ testBitNOfReg 6 c             -- BIT 6,C
+  , rr2op $ testBitNOfReg 6 d             -- BIT 6,D
+  , rr2op $ testBitNOfReg 6 e             -- BIT 6,E
+
+  , rr2op $ testBitNOfReg 6 h             -- BIT 6,H
+  , rr2op $ testBitNOfReg 6 l             -- BIT 6,L
+  , mr2op $ testBitNOfHLMem 6             -- BIT 6,(HL)
+  , rr2op $ testBitNOfReg 6 a             -- BIT 6,A
+
+  , rr2op $ testBitNOfReg 7 b             -- BIT 7,B
+  , rr2op $ testBitNOfReg 7 c             -- BIT 7,C
+  , rr2op $ testBitNOfReg 7 d             -- BIT 7,D
+  , rr2op $ testBitNOfReg 7 e             -- BIT 7,E
+
+  , rr2op $ testBitNOfReg 7 h             -- BIT 7,H
+  , rr2op $ testBitNOfReg 7 l             -- BIT 7,L
+  , mr2op $ testBitNOfHLMem 7             -- BIT 7,(HL)
+  , rr2op $ testBitNOfReg 7 a             -- BIT 7,A
+
+                                          -- 8x
+  , rr2op $ setBitNOfReg false 0 setB b   -- RES 0,B
+  , rr2op $ setBitNOfReg false 0 setC c   -- RES 0,C
+  , rr2op $ setBitNOfReg false 0 setD d   -- RES 0,D
+  , rr2op $ setBitNOfReg false 0 setE e   -- RES 0,E
+
+  , rr2op $ setBitNOfReg false 0 setH h   -- RES 0,H
+  , rr2op $ setBitNOfReg false 0 setL l   -- RES 0,L
+  , mm2op $ setBitNOfHLMem false 0        -- RES 0,(HL)
+  , rr2op $ setBitNOfReg false 0 setA a   -- RES 0,A
+
+  , rr2op $ setBitNOfReg false 1 setB b   -- RES 1,B
+  , rr2op $ setBitNOfReg false 1 setC c   -- RES 1,C
+  , rr2op $ setBitNOfReg false 1 setD d   -- RES 1,D
+  , rr2op $ setBitNOfReg false 1 setE e   -- RES 1,E
+
+  , rr2op $ setBitNOfReg false 1 setH h   -- RES 1,H
+  , rr2op $ setBitNOfReg false 1 setL l   -- RES 1,L
+  , mm2op $ setBitNOfHLMem false 1        -- RES 1,(HL)
+  , rr2op $ setBitNOfReg false 1 setA a   -- RES 1,A
+
+                                          -- 9x
+  , rr2op $ setBitNOfReg false 2 setB b   -- RES 2,B
+  , rr2op $ setBitNOfReg false 2 setC c   -- RES 2,C
+  , rr2op $ setBitNOfReg false 2 setD d   -- RES 2,D
+  , rr2op $ setBitNOfReg false 2 setE e   -- RES 2,E
+
+  , rr2op $ setBitNOfReg false 2 setH h   -- RES 2,H
+  , rr2op $ setBitNOfReg false 2 setL l   -- RES 2,L
+  , mm2op $ setBitNOfHLMem false 2        -- RES 2,(HL)
+  , rr2op $ setBitNOfReg false 2 setA a   -- RES 2,A
+
+  , rr2op $ setBitNOfReg false 3 setB b   -- RES 3,B
+  , rr2op $ setBitNOfReg false 3 setC c   -- RES 3,C
+  , rr2op $ setBitNOfReg false 3 setD d   -- RES 3,D
+  , rr2op $ setBitNOfReg false 3 setE e   -- RES 3,E
+
+  , rr2op $ setBitNOfReg false 3 setH h   -- RES 3,H
+  , rr2op $ setBitNOfReg false 3 setL l   -- RES 3,L
+  , mm2op $ setBitNOfHLMem false 3        -- RES 3,(HL)
+  , rr2op $ setBitNOfReg false 3 setA a   -- RES 3,A
+
+                                          -- Ax
+  , rr2op $ setBitNOfReg false 4 setB b   -- RES 4,B
+  , rr2op $ setBitNOfReg false 4 setC c   -- RES 4,C
+  , rr2op $ setBitNOfReg false 4 setD d   -- RES 4,D
+  , rr2op $ setBitNOfReg false 4 setE e   -- RES 4,E
+
+  , rr2op $ setBitNOfReg false 4 setH h   -- RES 4,H
+  , rr2op $ setBitNOfReg false 4 setL l   -- RES 4,L
+  , mm2op $ setBitNOfHLMem false 4        -- RES 4,(HL)
+  , rr2op $ setBitNOfReg false 4 setA a   -- RES 4,A
+
+  , rr2op $ setBitNOfReg false 5 setB b   -- RES 5,B
+  , rr2op $ setBitNOfReg false 5 setC c   -- RES 5,C
+  , rr2op $ setBitNOfReg false 5 setD d   -- RES 5,D
+  , rr2op $ setBitNOfReg false 5 setE e   -- RES 5,E
+
+  , rr2op $ setBitNOfReg false 5 setH h   -- RES 5,H
+  , rr2op $ setBitNOfReg false 5 setL l   -- RES 5,L
+  , mm2op $ setBitNOfHLMem false 5        -- RES 5,(HL)
+  , rr2op $ setBitNOfReg false 5 setA a   -- RES 5,A
+
+                                          -- Bx
+  , rr2op $ setBitNOfReg false 6 setB b   -- RES 6,B
+  , rr2op $ setBitNOfReg false 6 setC c   -- RES 6,C
+  , rr2op $ setBitNOfReg false 6 setD d   -- RES 6,D
+  , rr2op $ setBitNOfReg false 6 setE e   -- RES 6,E
+
+  , rr2op $ setBitNOfReg false 6 setH h   -- RES 6,H
+  , rr2op $ setBitNOfReg false 6 setL l   -- RES 6,L
+  , mm2op $ setBitNOfHLMem false 6        -- RES 6,(HL)
+  , rr2op $ setBitNOfReg false 6 setA a   -- RES 6,A
+
+  , rr2op $ setBitNOfReg false 7 setB b   -- RES 7,B
+  , rr2op $ setBitNOfReg false 7 setC c   -- RES 7,C
+  , rr2op $ setBitNOfReg false 7 setD d   -- RES 7,D
+  , rr2op $ setBitNOfReg false 7 setE e   -- RES 7,E
+
+  , rr2op $ setBitNOfReg false 7 setH h   -- RES 7,H
+  , rr2op $ setBitNOfReg false 7 setL l   -- RES 7,L
+  , mm2op $ setBitNOfHLMem false 7        -- RES 7,(HL)
+  , rr2op $ setBitNOfReg false 7 setA a   -- RES 7,A
+
+
+                                          -- Cx
+  , rr2op $ setBitNOfReg true 0 setB b    -- SET 0,B
+  , rr2op $ setBitNOfReg true 0 setC c    -- SET 0,C
+  , rr2op $ setBitNOfReg true 0 setD d    -- SET 0,D
+  , rr2op $ setBitNOfReg true 0 setE e    -- SET 0,E
+
+  , rr2op $ setBitNOfReg true 0 setH h    -- SET 0,H
+  , rr2op $ setBitNOfReg true 0 setL l    -- SET 0,L
+  , mm2op $ setBitNOfHLMem true 0         -- SET 0,(HL)
+  , rr2op $ setBitNOfReg true 0 setA a    -- SET 0,A
+
+  , rr2op $ setBitNOfReg true 1 setB b    -- SET 1,B
+  , rr2op $ setBitNOfReg true 1 setC c    -- SET 1,C
+  , rr2op $ setBitNOfReg true 1 setD d    -- SET 1,D
+  , rr2op $ setBitNOfReg true 1 setE e    -- SET 1,E
+
+  , rr2op $ setBitNOfReg true 1 setH h    -- SET 1,H
+  , rr2op $ setBitNOfReg true 1 setL l    -- SET 1,L
+  , mm2op $ setBitNOfHLMem true 1         -- SET 1,(HL)
+  , rr2op $ setBitNOfReg true 1 setA a    -- SET 1,A
+
+                                          -- Dx
+  , rr2op $ setBitNOfReg true 2 setB b    -- SET 2,B
+  , rr2op $ setBitNOfReg true 2 setC c    -- SET 2,C
+  , rr2op $ setBitNOfReg true 2 setD d    -- SET 2,D
+  , rr2op $ setBitNOfReg true 2 setE e    -- SET 2,E
+
+  , rr2op $ setBitNOfReg true 2 setH h    -- SET 2,H
+  , rr2op $ setBitNOfReg true 2 setL l    -- SET 2,L
+  , mm2op $ setBitNOfHLMem true 2         -- SET 2,(HL)
+  , rr2op $ setBitNOfReg true 2 setA a    -- SET 2,A
+
+  , rr2op $ setBitNOfReg true 3 setB b    -- SET 3,B
+  , rr2op $ setBitNOfReg true 3 setC c    -- SET 3,C
+  , rr2op $ setBitNOfReg true 3 setD d    -- SET 3,D
+  , rr2op $ setBitNOfReg true 3 setE e    -- SET 3,E
+
+  , rr2op $ setBitNOfReg true 3 setH h    -- SET 3,H
+  , rr2op $ setBitNOfReg true 3 setL l    -- SET 3,L
+  , mm2op $ setBitNOfHLMem true 3         -- SET 3,(HL)
+  , rr2op $ setBitNOfReg true 3 setA a    -- SET 3,A
+
+                                          -- Ex
+  , rr2op $ setBitNOfReg true 4 setB b    -- SET 4,B
+  , rr2op $ setBitNOfReg true 4 setC c    -- SET 4,C
+  , rr2op $ setBitNOfReg true 4 setD d    -- SET 4,D
+  , rr2op $ setBitNOfReg true 4 setE e    -- SET 4,E
+
+  , rr2op $ setBitNOfReg true 4 setH h    -- SET 4,H
+  , rr2op $ setBitNOfReg true 4 setL l    -- SET 4,L
+  , mm2op $ setBitNOfHLMem true 4         -- SET 4,(HL)
+  , rr2op $ setBitNOfReg true 4 setA a    -- SET 4,A
+
+  , rr2op $ setBitNOfReg true 5 setB b    -- SET 5,B
+  , rr2op $ setBitNOfReg true 5 setC c    -- SET 5,C
+  , rr2op $ setBitNOfReg true 5 setD d    -- SET 5,D
+  , rr2op $ setBitNOfReg true 5 setE e    -- SET 5,E
+
+  , rr2op $ setBitNOfReg true 5 setH h    -- SET 5,H
+  , rr2op $ setBitNOfReg true 5 setL l    -- SET 5,L
+  , mm2op $ setBitNOfHLMem true 5         -- SET 5,(HL)
+  , rr2op $ setBitNOfReg true 5 setA a    -- SET 5,A
+
+                                          -- Fx
+  , rr2op $ setBitNOfReg true 6 setB b    -- SET 6,B
+  , rr2op $ setBitNOfReg true 6 setC c    -- SET 6,C
+  , rr2op $ setBitNOfReg true 6 setD d    -- SET 6,D
+  , rr2op $ setBitNOfReg true 6 setE e    -- SET 6,E
+
+  , rr2op $ setBitNOfReg true 6 setH h    -- SET 6,H
+  , rr2op $ setBitNOfReg true 6 setL l    -- SET 6,L
+  , mm2op $ setBitNOfHLMem true 6         -- SET 6,(HL)
+  , rr2op $ setBitNOfReg true 6 setA a    -- SET 6,A
+
+  , rr2op $ setBitNOfReg true 7 setB b    -- SET 7,B
+  , rr2op $ setBitNOfReg true 7 setC c    -- SET 7,C
+  , rr2op $ setBitNOfReg true 7 setD d    -- SET 7,D
+  , rr2op $ setBitNOfReg true 7 setE e    -- SET 7,E
+
+  , rr2op $ setBitNOfReg true 7 setH h    -- SET 7,H
+  , rr2op $ setBitNOfReg true 7 setL l    -- SET 7,L
+  , mm2op $ setBitNOfHLMem true 7         -- SET 7,(HL)
+  , rr2op $ setBitNOfReg true 7 setA a    -- SET 7,A
 ]
